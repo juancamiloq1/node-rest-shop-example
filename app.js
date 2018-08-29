d2bb091fa07efd0b0b0ffa260db1b4b9c37bf97e
JuanCamiloQHz <juancamiloqhz@gmail.com>
2018-08-29 11:56:23 -0500

    preventing cors errors. also adding body parser

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const productsRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// CORS setup into middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT",
+      "POST",
+      "PATCH",
+      "DELETE"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // Routes which should handle requests
 app.use("/products", productsRoutes);
 app.use("/orders", ordersRoutes);
 
-// Aqui se define custom error message en not found routes
+// define custom error message on not found routes
 app.use((req, res, next) => {
   const error = new Error("Not Found");
   error.status = 404;
