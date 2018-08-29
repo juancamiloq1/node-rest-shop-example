98951741083f9e228aa6c64cbd05b0a79a3d3c0f
JuanCamiloQHz <juancamiloqhz@gmail.com>
2018-08-29 11:31:06 -0500

    end of 4th video. Errors and nodemon and morgan

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+// Handle incomming GET requests to /orders
 router.get("/", (req, res, next) => {
   res.status(200).json({
     message: "Orders were fetched"
--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
 const productsRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
 
-const app = express();
+app.use(morgan("dev"));
 
+// Routes which should handle requests
 app.use("/products", productsRoutes);
 app.use("/orders", ordersRoutes);
+
+// Aqui se define custom error message en not found routes
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// Error when doing stuff into the database (when route exists)
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
